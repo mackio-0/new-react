3731f98db1969ae0600bd14a5ed9220a4965cecc
mackio-0 <minkko000@gmail.com>
2024-10-27 15:20:35 +0630

    Chore: set up dependencies and folder structure Chore: create barebone files Chore: set up router

---
+++ 13-voucher-app/src/pages/VoucherPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VoucherPage = () => {
+  return (
+    <div>
+      Voucher
+    </div>
+  )
+}
+
+export default VoucherPage
---
+++ 13-voucher-app/src/router.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import NotFoundPage from "./pages/NotFoundPage";
+import DashboardPage from "./pages/DashboardPage";
+import ProductPage from "./pages/ProductPage";
+import SalePage from "./pages/SalePage";
+import VoucherPage from "./pages/VoucherPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        errorElement: <NotFoundPage/>,
+        children: [
+            {
+                path: "/",
+                element: <DashboardPage/>
+            },
+            {
+                path: "/product",
+                element: <ProductPage/>
+            },
+            {
+                path: "/sale",
+                element: <SalePage/>
+            },
+            {
+                path: "/voucher",
+                element: <VoucherPage/>
+            }
+        ],
+    }
+]);
+
+export default router;
