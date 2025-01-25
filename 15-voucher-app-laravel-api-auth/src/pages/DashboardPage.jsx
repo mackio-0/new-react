e19c00aa79dd27d2e7e9c0d65b10b4991a0c90e6
mackio-0 <minkko000@gmail.com>
2025-01-26 01:56:31 +0630

    Update: context.txt

--- 15-voucher-app-laravel-api-auth/src/pages/DashboardPage.jsx
+++ 15-voucher-app-laravel-api-auth/src/pages/DashboardPage.jsx
@@ -8,32 +8,32 @@ import {
 } from "react-icons/hi2";
 
 const DashboardPage = () => {
+
   return (
     <section>
-
-        <div className="grid md:grid-cols-3 grid-rows-3 gap-5">
-          <div className="col-span-1 row-span-1">
-            <ModuleBtn
-              url={"/dashboard/product"}
-              name={"Product Module"}
-              icon={<HiCircleStack className="size-14" />}
-            />
-          </div>
-          <div className="col-span-1 row-span-1">
-            <ModuleBtn
-              url={"/dashboard/sale"}
-              name={"Sale Module"}
-              icon={<HiMiniComputerDesktop className="size-14" />}
-            />
-          </div>
-          <div className="col-span-1 row-span-1">
-            <ModuleBtn
-              url={"/dashboard/voucher"}
-              name={"Voucher Module"}
-              icon={<HiDocumentDuplicate className="size-14" />}
-            />
-          </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 grid-rows-3 gap-5">
+        <div className="col-span-1 row-span-1">
+          <ModuleBtn
+            url={"/dashboard/product"}
+            name={"Product Module"}
+            icon={<HiCircleStack className="size-14" />}
+          />
         </div>
+        <div className="col-span-1 row-span-1">
+          <ModuleBtn
+            url={"/dashboard/sale"}
+            name={"Sale Module"}
+            icon={<HiMiniComputerDesktop className="size-14" />}
+          />
+        </div>
+        <div className="col-span-1 row-span-1">
+          <ModuleBtn
+            url={"/dashboard/voucher"}
+            name={"Voucher Module"}
+            icon={<HiDocumentDuplicate className="size-14" />}
+          />
+        </div>
+      </div>
     </section>
   );
 };
